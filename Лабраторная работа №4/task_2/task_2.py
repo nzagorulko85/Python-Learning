a5247d350a7539daa2ccddb8c01ad9c6b17a5791
nastyaZZZ <n.zagorulko85@gmail.com>
2026-01-19 22:20:35 +0300

    Add files via upload

---
+++ Лабраторная работа №4/task_2/task_2.py
@@ -0,0 +1,24 @@
+# TODO импортировать необходимые молули
+
+import csv
+import json
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    with open(INPUT_FILENAME) as file:
+        reader = csv.DictReader(file)
+        data = list(reader)
+    with open(OUTPUT_FILENAME, "w") as file:
+        json.dump(data, file, indent=4)
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
